1d8bd636225f4c058ff21809e453087db59cc629
YooHyeokSchool <webdevyoo@gmail.com>
2024-07-11 16:18:45 +0900

    feat: 기본 layout 적용 및 todoList배열 데이터 push ($emit 데이터 공유)

---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+   <v-layout row wrap><!-- row wrap : vuetify의 그리드 시스템 활용 -->
+    <v-flex xs6>
+      <div>{{ todoList }}</div>
+      <List/>
+    </v-flex>
+    <v-flex xs6><!-- xs6 = 화면 절반차지: vuetify의 그리드 시스템은 flex-box기반 가로 기준 12개로 나누어 구성됨.-->
+      <ListAdd
+        @listAdd="listAdd"
+      />
+    </v-flex>
+   </v-layout>
+  </v-container>
+</template>
+
+<script>
+import List from './List'
+import ListAdd from './ListAdd'
+
+export default {
+  components: {
+    List,
+    ListAdd
+  },
+  data() {
+    return {
+      todoList: []
+    }
+  },
+  methods: {
+    listAdd(memo) {
+      this.todoList.push({
+        memo, 
+        status: 'created'
+      })
+      console.log(this.todoList)
+    }
+  },
+}
+</script>
---
+++ src/components/ListAdd.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <!-- 텍스트 필드 -->
+        <v-textarea 
+        outline
+        v-model="memo"
+        label="투두리스트를 입력해 주세요"
+        value=""
+        ></v-textarea>
+        <!-- 추가 버튼 -->
+        <v-btn @click="listAdd">리스트 추가</v-btn>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            memo : null
+        }
+    },
+    methods: {
+        listAdd() {
+            console.log("리스트 추가")
+            if (!this.memo) {
+                alert('할 일을 입력해 주세요')
+                return;
+            }
+            this.$emit("listAdd", this.memo)
+        }
+    }
+}
+</script>
