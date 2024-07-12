2b10da87c16d5f9d4090e5c89bb2a364d2a320db
YooHyeokSchool <webdevyoo@gmail.com>
2024-07-12 10:43:45 +0900

    feat: List 컴포넌트 template 구현 및 Home(부모) 컴포넌트로부터 todoList props 전달

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <v-container>
    <v-layout row wrap><!-- row wrap : vuetify의 그리드 시스템 활용 -->
-    <v-flex xs6>
-      <div>{{ todoList }}</div>
-      <List/>
+    <v-flex xs6 pa-2>
+      <List
+        :todoList="todoList"
+      />
     </v-flex>
-    <v-flex xs6><!-- xs6 = 화면 절반차지: vuetify의 그리드 시스템은 flex-box기반 가로 기준 12개로 나누어 구성됨.-->
+    <v-flex xs6 pa-2><!-- xs6 = 화면 절반차지: vuetify의 그리드 시스템은 flex-box기반 가로 기준 12개로 나누어 구성됨.-->
+      <!-- @listAdd : $emit의 Listener 역할 -->
       <ListAdd
-        @listAdd="listAdd"
+        @listAdd="listAdd" 
       />
     </v-flex>
    </v-layout>
@@ -16,7 +18,7 @@
 
 <script>
 import List from './List'
-import ListAdd from './ListAdd'
+import ListAdd from './ListAdd.vue'
 
 export default {
   components: {
@@ -34,7 +36,6 @@ export default {
         memo, 
         status: 'created'
       })
-      console.log(this.todoList)
     }
   },
 }
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,5 +1,23 @@
 <template>
     <div>
-        투두 리스트
+        <!-- 동일하 레이아웃이 여러개 반복되기 위해 v-for 속성 사용 -->
+        <!-- key는 중복될 수 있으므로 index를 사용한다. (수정/삭제시 필수) -->
+        <v-card
+            class="pa-3"
+            v-for="(list, index) in todoList"
+            :key="index"
+        >
+            <p>{{list.memo}}</p> <!-- 할일 내용 -->
+            <v-btn fab text x-small color="green"><i class="fas fa-check">완료</i></v-btn>
+            <v-btn fab text x-small color="blue"><i class="fas fa-redo-alt">부활</i></v-btn>
+            <v-btn fab text x-small color="red"><i class="fas fa-trash-alt">제거</i></v-btn>
+        </v-card>
     </div>
 </template>
+<script>
+
+export default {
+    props: ['todoList'],
+
+}
+</script>
